b1b24d73523a4b4feae9cdf9b005a1ec7011ee16
mwiewior <marek.wiewiorka@gmail.com>
2020-10-15 20:30:08 +0200

    Adding landing page build

--- build_docs.sh
+++ build_docs.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-cd docs
 echo $VERSION
-if  echo $VERSION | grep -Eq "SNAPSHOT$"; then
-    docker build --no-cache -t biodatageeks/$CI_PROJECT_NAME-snap-doc .
-    if [ $(docker ps | grep $CI_PROJECT_NAME-snap-doc | wc -l) -gt 0 ]; then docker stop $CI_PROJECT_NAME-snap-doc && docker rm $CI_PROJECT_NAME-snap-doc; fi
-    docker run -p ${DOC_PORT_PREFIX}81:80 -d --name $CI_PROJECT_NAME-snap-doc biodatageeks/$CI_PROJECT_NAME-snap-doc
-else
-    docker build --no-cache -t biodatageeks/$CI_PROJECT_NAME-doc .
-    if [ $(docker ps | grep $CI_PROJECT_NAME-doc | wc -l) -gt 0 ]; then docker stop $CI_PROJECT_NAME-doc && docker rm $CI_PROJECT_NAME-doc; fi
-    docker run -d -p ${DOC_PORT_PREFIX}80:80 --name $CI_PROJECT_NAME-doc biodatageeks/$CI_PROJECT_NAME-doc
-fi
 
+function build_run() {
+  CONTAINER_SUFIX=$1
+  if  echo $VERSION | grep -Eq "SNAPSHOT$"; then
+      docker build --no-cache -t biodatageeks/$CI_PROJECT_NAME-snap-$CONTAINER_SUFIX .
+      if [ $(docker ps | grep $CI_PROJECT_NAME-snap-$CONTAINER_SUFIX | wc -l) -gt 0 ]; then docker stop $CI_PROJECT_NAME-snap-$CONTAINER_SUFIX && docker rm $CI_PROJECT_NAME-snap-$CONTAINER_SUFIX; fi
+      docker run -p ${DOC_PORT_PREFIX}81:80 -d --name $CI_PROJECT_NAME-snap-$CONTAINER_SUFIX biodatageeks/$CI_PROJECT_NAME-snap-$CONTAINER_SUFIX
+  else
+      docker build --no-cache -t biodatageeks/$CI_PROJECT_NAME-$CONTAINER_SUFIX .
+      if [ $(docker ps | grep $CI_PROJECT_NAME-$CONTAINER_SUFIX | wc -l) -gt 0 ]; then docker stop $CI_PROJECT_NAME-$CONTAINER_SUFIX && docker rm $CI_PROJECT_NAME-$CONTAINER_SUFIX; fi
+      docker run -d -p ${DOC_PORT_PREFIX}80:80 --name $CI_PROJECT_NAME-$CONTAINER_SUFIX biodatageeks/$CI_PROJECT_NAME-$CONTAINER_SUFIX
+  fi
+}
 
+cd docs
+build_run "doc"
+
+cd ../page
+build_run "page"
