23036891ed7ecb49b4117bc7d97d8efb89b6e30b
mwiewior <marek.wiewiorka@gmail.com>
2020-10-24 11:13:48 +0200

    Adding autostart for containers

--- build_docs.sh
+++ build_docs.sh
@@ -9,11 +9,11 @@ function build_run() {
   if  echo $VERSION | grep -Eq "SNAPSHOT$"; then
       docker build --no-cache -t biodatageeks/$CI_PROJECT_NAME-snap-$CONTAINER_SUFIX .
       if [ $(docker ps | grep $CI_PROJECT_NAME-snap-$CONTAINER_SUFIX | wc -l) -gt 0 ]; then docker stop $CI_PROJECT_NAME-snap-$CONTAINER_SUFIX && docker rm $CI_PROJECT_NAME-snap-$CONTAINER_SUFIX; fi
-      docker run -p ${DOC_PORT_PREFIX}$PORT_SNAP:80 -d --name $CI_PROJECT_NAME-snap-$CONTAINER_SUFIX biodatageeks/$CI_PROJECT_NAME-snap-$CONTAINER_SUFIX
+      docker run --restart unless-stopped -p ${DOC_PORT_PREFIX}$PORT_SNAP:80 -d --name $CI_PROJECT_NAME-snap-$CONTAINER_SUFIX biodatageeks/$CI_PROJECT_NAME-snap-$CONTAINER_SUFIX
   else
       docker build --no-cache -t biodatageeks/$CI_PROJECT_NAME-$CONTAINER_SUFIX .
       if [ $(docker ps | grep $CI_PROJECT_NAME-$CONTAINER_SUFIX | wc -l) -gt 0 ]; then docker stop $CI_PROJECT_NAME-$CONTAINER_SUFIX && docker rm $CI_PROJECT_NAME-$CONTAINER_SUFIX; fi
-      docker run -d -p ${DOC_PORT_PREFIX}$PORT_PROD:80 --name $CI_PROJECT_NAME-$CONTAINER_SUFIX biodatageeks/$CI_PROJECT_NAME-$CONTAINER_SUFIX
+      docker run --restart unless-stopped -d -p ${DOC_PORT_PREFIX}$PORT_PROD:80 --name $CI_PROJECT_NAME-$CONTAINER_SUFIX biodatageeks/$CI_PROJECT_NAME-$CONTAINER_SUFIX
   fi
 }
 
