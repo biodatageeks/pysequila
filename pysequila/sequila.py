2d50f9abf431e27f68c9f8670659317d4afe079f
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2021-11-25 14:45:24 +0100

    Cleanin up for sequila 0.7.0

--- pysequila/sequila.py
+++ pysequila/sequila.py
@@ -12,11 +12,7 @@ def register(session: SparkSession):
     """
     assert check_argument_types()
     # pylint: disable=W0212
-    spark_session = (
-        session._jvm.org.apache.spark.sql.SparkSession.builder()
-        .enableHiveSupport()
-        .getOrCreate()
-    )
+    spark_session = session._jvm.org.apache.spark.sql.SparkSession.builder().enableHiveSupport().getOrCreate()
 
     seq_session = session._jvm.org.apache.spark.sql.SequilaSession(spark_session)
 
@@ -24,20 +20,13 @@ def register(session: SparkSession):
     session._jvm.org.biodatageeks.sequila.utils.UDFRegister.register(seq_session)
 
 
-class SequilaSession(SparkSession):
+class SequilaSession(SparkSession):  # pylint: disable=too-few-public-methods
     """Wrapper for SparkSession."""
 
     def __init__(self, session: SparkSession, jsparkSession=None):
         """Create a new SequilaSession."""
         SparkSession.__init__(self, session.sparkContext)
-        seq_session = session._jvm.org.apache.spark.sql.SequilaSession(
-            session._jsparkSession
-        )  # pylint: disable=W0212
-
-        session._jvm.org.biodatageeks.sequila.utils.SequilaRegister.register(
-            seq_session
-        )
-        session._jvm.org.biodatageeks.sequila.utils.UDFRegister.register(seq_session)
+        seq_session = session._jvm.org.apache.spark.sql.SequilaSession(session._jsparkSession)  # pylint: disable=W0212
         session._jvm.SequilaSession.setDefaultSession(seq_session)
         sequila_session = SequilaSession._instantiatedSession
 
@@ -48,10 +37,7 @@ class SequilaSession(SparkSession):
         if jsparkSession is None:
             if (
                 self._jvm.SequilaSession.getDefaultSession().isDefined()
-                and not self._jvm.SequilaSession.getDefaultSession()
-                .get()
-                .sparkContext()
-                .isStopped()
+                and not self._jvm.SequilaSession.getDefaultSession().get().sparkContext().isStopped()
             ):
                 jsparkSession = self._jvm.SequilaSession.getDefaultSession().get()
             else:
@@ -59,9 +45,6 @@ class SequilaSession(SparkSession):
         self._jsparkSession = jsparkSession
         self._jwrapped = self._jsparkSession.sqlContext()
         self._wrapped = SQLContext(self._sc, self, self._jwrapped)
-        if (
-            SequilaSession._instantiatedSession is None
-            or SequilaSession._instantiatedSession._sc._jsc is None
-        ):
+        if SequilaSession._instantiatedSession is None or SequilaSession._instantiatedSession._sc._jsc is None:
             SequilaSession._instantiatedSession = self
             self._jvm.SparkSession.setDefaultSession(self._jsparkSession)
